84c480a573e49712797ea67291cebb33dc1f3a78
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-03-02 01:50:20 +0530

    Add drawer component with animated transitions; update layout to include home tab icon

--- ecom-mobile/app/_layout.tsx
+++ ecom-mobile/app/_layout.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Stack, Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function _layout() {
-    return <GluestackUIProvider mode="light"><Tabs /></GluestackUIProvider>;
+    return <GluestackUIProvider mode="light">
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" size={size} color={'#000'} />
+                    )
+                }}
+            />
+        </Tabs>
+    </GluestackUIProvider>;
 }
